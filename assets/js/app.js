8230bb0ba640c3b4ed3b675dfb01ed125993ffce
balbini <balbini91@gmail.com>
2017-08-19 20:26:45 -0700

    more updates and img adds

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,8 +5,9 @@ $(document).ready(function(){
   let objectArray = [
     {
       name: "My First Project",
-      project_name: "Google",//"https://www.google.com/",
-      description: "A Search Engine"
+      project_name: "Google",
+      description: "A Search Engine",
+      project_image: "https://www.google.com/homepage/images/hero-dhp-chrome-mac.jpg?mmfb=9f871c66de344d37d2dd5a8c74d03a1b"
     }];
     let objectArray_2 = [
       {
@@ -28,17 +29,20 @@ $(document).ready(function(){
         $(".project_links").append(`<li>${objectArray[i].name}</li>`)
         $(".project_links").append(`<li>${objectArray[i].project_name}</li>`)
         $(".project_links").append(`<li>${objectArray[i].description}</li>`)
-      },
+        $(".img_1").attr("src", `${objectArray[i].project_image}`)
+      }
       for (i = 0; i < objectArray.length; i++){
         $(".project_links_2").append(`<li>${objectArray_2[i].name}</li>`)
         $(".project_links_2").append(`<li>${objectArray_2[i].project_name}</li>`)
         $(".project_links_2").append(`<li>${objectArray_2[i].description}</li>`)
-      },
+        $(".img_2").attr("src", `${objectArray_2[i].project_image}`)
+      }
       for (i = 0; i < objectArray.length; i++){
         $(".project_links_3").append(`<li>${objectArray_3[i].name}</li>`)
         $(".project_links_3").append(`<li>${objectArray_3[i].project_name}</li>`)
         $(".project_links_3").append(`<li>${objectArray_3[i].description}</li>`)
       };
+
 
       //add image to array
       //add image to for loop
